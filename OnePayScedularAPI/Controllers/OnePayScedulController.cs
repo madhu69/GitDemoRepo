903bdebf265ac0efd8b2626db9f89ad118dc6f7f
Madhu <madhusudan7296@gmail.com>
2022-09-07 16:01:46 +0530

    Archi america

--- OnePayScedularAPI/Controllers/OnePayScedulController.cs
+++ OnePayScedularAPI/Controllers/OnePayScedulController.cs
@@ -32,11 +32,8 @@ namespace OnePayScedularAPI.Controllers
             
         };
 
-<<<<<<< HEAD
-private readonly ILogger<OnePayScedulController> _logger;
-=======
+
         private readonly ILogger<OnePayScedulController> _logger;
->>>>>>> ce34da393add9b2bf17e862490cf67bcc3626bd4
 
         public OnePayScedulController(ILogger<OnePayScedulController> logger)
         {
