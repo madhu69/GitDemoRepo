658af703ea22a2252bf647c613a4c114f30010ab
Madhu <madhusudan7296@gmail.com>
2022-09-07 12:43:04 +0530

    commit from Asia

--- OnePayScedularAPI/Controllers/OnePayScedulController.cs
+++ OnePayScedularAPI/Controllers/OnePayScedulController.cs
@@ -13,6 +13,8 @@ namespace OnePayScedularAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
